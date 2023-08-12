b05a0d8d0e736d87d8710758e23f68606bed61c6
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-12 15:52:07 +0530

    syntatic sugars

--- src/main/scala/scala3byexamples/section1as/Recap.scala
+++ src/main/scala/scala3byexamples/section1as/Recap.scala
@@ -81,17 +81,71 @@ object Recap {
       // code that may fail
       throwException()
     } catch {
-      case e: Exception => println ("I caught an exception")
+      case e: Exception => println("I caught an exception")
     } finally {
       // closing resources
       println("Some important logs")
     }
 
-  //functional programming
+  // functional programming
+  val square = new Function1[Int, Int] {
+    override def apply(v1: Int): Int = v1 * v1
+  }
+  // different notation of anonymous function
+  val increament = new ((Int) => Int) {
+    override def apply(v1: Int): Int = v1 + 1
+  }
+
+  val four = square(2)
+  val five = increament(4)
+
+  // lamdas
+  val anonymousSquare = (x: Int) => x + 1
+
+  // higher order functions
+  val squaredList = List(1, 2, 3).map(anonymousSquare)
+  // map, flatMap, filter
+
+  // for-compression
+  val oddEvenPair = for {
+    num1 <- List(25, 35, 42, 36, 31, 14, 15) if num1 % 2 == 0
+    num2 <- List(25, 35, 42, 36, 31, 14, 15) if num2 % 2 == 1
+  } yield s"$num1- $num2"
+
+  // Scalla collections: Seqs, Arrays, List, Vectors, Maps, Tuples, Sets
+
+  // options
+  val anOption: Option[Int] = Option(42)
+
+  // pattern matching
+
+  val hari = Employee("Hari", 123)
+
+  val welcome = hari match {
+    case Employee(n, _) => s"Welcome $n"
+  }
+
+//brace less syntax for scala 3
+//same for if, match, while
+  val oddEvenPair_v2 =
+    for
+      num1 <- List(25, 35, 42, 36, 31, 14, 15) if num1 % 2 == 0
+      num2 <- List(25, 35, 42, 36, 31, 14, 15) if num2 % 2 == 1
+    yield s"$num1- $num2"
+
+//identation token
+  class BracelessAnimal:
+    def eat(): Unit =
+      println("I am doing something")
+      println("I am  eating")
+    end eat //optional
+  end BracelessAnimal //optional
 
   def main(args: Array[String]): Unit = {
     println(fact5)
     bTiger.eat(bTiger)
+    println(five)
+    println(oddEvenPair)
   }
 
 }
---
+++ src/main/scala/scala3byexamples/section1as/ScalaSyntaticSugars.scala
@@ -0,0 +1,124 @@
+package scala3byexamples.section1as
+
+import scala.util.Try
+
+object ScalaSyntaticSugars {
+
+  // a- methods with one argument
+
+  def singleArgMethod(n: Int): Int = n + 1
+
+  val aMethodCall = singleArgMethod({
+    // long code
+    42
+  })
+
+  val aMethodCall_v2 = singleArgMethod {
+    // long code
+    42
+  }
+//example: Try, Future
+
+  val aTryInstance = Try {
+    throw new RuntimeException
+  }
+
+  // with hofs
+  val squaredList = List(1, 2, 3, 4).map { x =>
+    // code block
+    x * x
+  }
+
+  // b - single abstract method pattern
+
+  trait Action {
+    def act(x: Int): Int
+  }
+
+  val anAction = new Action:
+    // can aslo have other implemented fields/methods here
+    override def act(x: Int): Int = x * x
+
+  val anotherAction: Action =
+    (x: Int) => x + 1 // new Action { def act(x: Int) =  x + 1 }
+
+  // anotherAction() //error
+  val res = anotherAction.act(2)
+
+  // example: Runnable
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = println("from another thread")
+  })
+
+  val aBetterThread = new Thread(() => println("from another thread"))
+
+  // c - method ending in : are right asociative
+
+  val aList = 0 :: List(1, 2, 3)
+  val sameList = List(1, 2, 3).::(0)
+
+  val anotherList = 0 :: 1 :: 2 :: List(3, 4)
+  val sameAnotherList = List(3, 4).::(2).::(1).::(0)
+
+  class MyStream[T] {
+    infix def -->:(value: T): MyStream[T] = this // implementation not important
+  }
+
+  val myStream = 1 -->: 2 -->: 2 -->: 3 -->: new MyStream[Int]
+
+  // d - multi word identifier
+
+  class Talker(name: String) {
+    infix def `and then said`(gossip: String) = println(s"$name siad $gossip")
+  }
+
+  val sabuj = new Talker("Sabuj Kumar Jena")
+  val sabujStatement = sabuj `and then said` "I love Scala 3"
+
+  // example: HTTP libraries
+  object `Contenet-Type` {
+    val `application/json` = "application/JSON"
+  }
+
+  // e - infix types
+  import scala.annotation.targetName
+  @targetName("Arrpw") // for more readable bytecode + Java interop
+  infix class -->[A, B]
+  val compositeType: -->[Int, String] = new -->[Int, String]
+  val compositeType_v2: Int --> String = new -->[Int, String] // infix notation
+
+  // f - update()
+  val anArray = Array(20, 30, 40)
+  anArray.update(1, 3)
+  anArray(0) = 200
+
+  // g - mutable fields
+  class Mutable {
+    private var internalMember: Int = 0
+    def member = internalMember // "getter" in java
+    def member_=(value: Int): Unit =
+      internalMember = value // "setter" in java
+  }
+
+  val aMutableContainer = new Mutable
+  aMutableContainer.member = 42 // aMutableContainer.member_=(42)
+
+  // h - variable arguments (varargs)
+  def methodWithVarargs(args: Int*) = {
+    // returns the number of arguments supplied
+    args.length
+  }
+
+  val callWithZeroArgs = methodWithVarargs()
+  val callWithOneArg = methodWithVarargs(23)
+  val callWithTwoArgs = methodWithVarargs(34, 52)
+
+  val aCollection = List(1, 2, 3, 4)
+
+  val callWithDynamicArgs = methodWithVarargs(aCollection*)
+  def main(args: Array[String]): Unit = {
+    println(anArray.toList)
+    println(callWithZeroArgs)
+  }
+
+}
