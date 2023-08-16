01d39c8e4954bcc5525a085397f0413c60a8f13e
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-16 13:44:01 +0530

    contextual abstraction part 1

---
+++ src/main/scala/scala3byexamples/section4/Givens.scala
@@ -0,0 +1,99 @@
+package scala3byexamples.section4
+
+object Givens {
+
+  // custom sorting
+  case class Employee(name: String, age: Int)
+
+  val employees =
+    List(Employee("Sabuj", 43), Employee("Bob", 49), Employee("Martin", 25))
+  given employeeOrdering: Ordering[Employee] = new Ordering[Employee]:
+    override def compare(x: Employee, y: Employee): Int =
+      x.name.compareTo(y.name)
+
+  val sortedEmployees =
+    employees.sorted // (employeeOrdering) <-- automatically passed by the compiler
+
+  object EmployeeAltSyntax {
+    given employeeOrdering: Ordering[Employee] with {
+      override def compare(x: Employee, y: Employee): Int =
+        x.name.compareTo(y.name)
+    }
+  }
+
+  // using clause
+  trait Combinator[A] {
+    def combine(x: A, y: A): A
+  }
+
+  def combineAll[A](list: List[A])(using combinator: Combinator[A]): A =
+    list.reduce(combinator.combine)
+  /*
+  combineAll(list(1,2,3,4))
+  combineAll(employees)
+   */
+  given intCombinator: Combinator[Int] with {
+    override def combine(x: Int, y: Int): Int = x + y
+  }
+
+  val firstSum = combineAll(List(1, 2, 3, 4))
+
+  // val combineAllEmployees = combineAll(employees) // doesn't compile - no Combinator[Employee] in scope
+
+  // context bound
+  def combineInGroupOf3[A](
+      list: List[A]
+  )(using Combinator[A]): List[A] =
+    list
+      .grouped(3)
+      .map(group =>
+        combineAll(group) /* given Combinator[A] passed by the compiler*/
+      )
+      .toList
+
+  // context bound
+  def combineInGroupOf3_v2[A: Combinator](
+      list: List[A]
+  ): List[A] = // A :Combiner => there is a given Combinator[A] in scope
+    list
+      .grouped(3)
+      .map(group =>
+        combineAll(group) /* given Combinator[A] passed by the compiler*/
+      )
+      .toList
+
+  val gruopedSum = combineInGroupOf3_v2(List(1, 2, 3, 4, 5))
+
+  // synthesize new given instances based on existing ones
+//  given listOrdering(using intOrdering: Ordering[Int]): Ordering[List[Int]]
+//  with {
+//    override def compare(x: List[Int], y: List[Int]): Int = x.sum - y.sum
+//  }
+
+  // ... with generics
+  given listOrderingBasedOnCombinator[A](using
+      ordering: Ordering[A]
+  )(using Combinator[A]): Ordering[List[A]] with {
+    override def compare(x: List[A], y: List[A]) =
+      ordering.compare(combineAll(x), combineAll(y))
+  }
+
+  val listOfLists = List(List(1, 2), List(1, 1), List(3, 4, 5))
+
+  val nestedListsOrdered = listOfLists.sorted
+
+  // pass a regular value instead of a given
+
+  val myCombinator = new Combinator[Int] {
+    override def combine(x: Int, y: Int) = x * y
+  }
+  val listProduct = combineAll(List(2,3,4,5))(using myCombinator)
+  def main(args: Array[String]): Unit = {
+    println(listProduct)
+    println(nestedListsOrdered)
+    println(gruopedSum)
+    println(firstSum)
+    println(sortedEmployees)
+  }
+
+}
