1e25c21ea63a31113226de9aa8d7bf565682010d
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-17 21:53:11 +0530

    legacy implicits

--- src/main/scala/scala3byexamples/section4/Implicits.scala
+++ src/main/scala/scala3byexamples/section4/Implicits.scala
@@ -4,21 +4,76 @@ object Implicits {
 
   // given/using clauses
   // the ability to pass arguments automatically (implicitly) by the compiler
+
+  // implicit arg -> using arg
+  // implicit val -> given
   trait Semigroup[A] {
     def combine(x: A, y: A): A
   }
 
-  def combineAll[A](list: List[A])(using semigroup: Semigroup[A]): A =
+  def combineAll[A](list: List[A])(implicit semigroup: Semigroup[A]): A =
     list.reduce(semigroup.combine)
 
-  given intSemigroup: Semigroup[Int] with
+  implicit val intSemigroup: Semigroup[Int] = new Semigroup[Int]:
     override def combine(x: Int, y: Int): Int = x + y
 
   val sumOf10 = combineAll((1 to 10).toList)
   // extension methods
-  // implicit conversion
+//  extension  (number: Int)
+//    def isEven = number % 2 == 0
+  // extension methods = implicit class
+
+  implicit class MyInteger(num: Int) {
+    // extension methods here
+    def isEven = num % 2 == 0
+  }
+  val is25Even = 25.isEven // new MyInt(25).isEven
+
+  // implicit conversion - SUPER DANGEROUS, difficult to debug
+  case class Employee(name: String) {
+    def greet(): String = s"Hi, I'm $name"
+  }
+
+  implicit def string2Person(x: String): Employee = Employee(
+    x
+  ) // implicit conversion
+  val bob = Employee("Bob")
+  val bobSaysHi = bob.greet()
+  val bobSaysHi_v2 =
+    "bob".greet() // string2Person("bob").greet()
+
+  // the goal of "implicit def" was too synthesize NEW implicit values
+
+  implicit def semigroupOfOption[A](implicit
+      semigroup: Semigroup[A]
+  ): Semigroup[Option[A]] =
+    new Semigroup[Option[A]] {
+      override def combine(x: Option[A], y: Option[A]): Option[A] = for {
+        valueX <- x
+        valueY <- y
+      } yield semigroup.combine(valueX, valueY)
+    }
+
+//  given semigroupOfOption[A](using
+//      semigroup: Semigroup[A]
+//  ): Semigroup[Option[A]] with
+//    override def combine(x: Option[A], y: Option[A]): Option[A] = for {
+//      valueX <- x
+//      valueY <- y
+//    } yield semigroup.combine(valueX, valueY)
+
+// AMBIGUOUS  between implicit conversion and implicit defination (to synthesize new implicit values)
+  /*
+  Why implicits will be phased out:
+    - the implicit keyword has many different meanings
+    - conversions are easy to abuse
+    - implicits are very hard to track down while debugging (givens also not trivial, but they are explicitly imported
+   */
   def main(args: Array[String]): Unit = {
     println(sumOf10)
+    println(is25Even)
+    println(bobSaysHi)
+    println(bobSaysHi_v2)
   }
 
 }
