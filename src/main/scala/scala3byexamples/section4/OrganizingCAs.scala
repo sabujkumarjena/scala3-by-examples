bc18b5610e098c331412c19d4de63a2e2022777a
sabuj <sabuj.kumar.jena@gmail.com>
2023-08-17 13:51:13 +0530

    Type classes

--- src/main/scala/scala3byexamples/section4/OrganizingCAs.scala
+++ src/main/scala/scala3byexamples/section4/OrganizingCAs.scala
@@ -19,8 +19,7 @@ object OrganizingCAs {
     given employeeAgeOrdering: Ordering[Employee] with
       override def compare(x: Employee, y: Employee): Int = y.age - x.age
 
-    extension (e: Employee)
-      def greet: String = s"Hello, I am ${e.name}"
+    extension (e: Employee) def greet: String = s"Hello, I am ${e.name}"
   }
 
   // a - import explicitly
@@ -56,39 +55,41 @@ object OrganizingCAs {
   3) When you have MANY possible givens and NO ONE is dominant, add them in separate objects and import them explicitly.
    */
 
-  //Same principles apply to extension methods as well.
+  // Same principles apply to extension methods as well.
 
-  /**
-   * Exercises. Create given instances for Orrdering[Purchase]
-   *  - ordering by total price, descending = 50 % of code base
-   *  - ordering by unit count, descending = 25% of code base
-   *  - ordering by unit price, ascending = 25% of code base
-   */
+  /** Exercises. Create given instances for Orrdering[Purchase]
+    *   - ordering by total price, descending = 50 % of code base
+    *   - ordering by unit count, descending = 25% of code base
+    *   - ordering by unit price, ascending = 25% of code base
+    */
 
-    case class Purchase(nUnits: Int, unitPrice: Double)
+  case class Purchase(nUnits: Int, unitPrice: Double)
   object Purchase {
     given purchaseOrdering: Ordering[Purchase] with
-      override def compare(x: Purchase, y: Purchase): Int = (y.nUnits * y.unitPrice - x.nUnits * x.unitPrice).asInstanceOf[Int]
+      override def compare(x: Purchase, y: Purchase): Int =
+        (y.nUnits * y.unitPrice - x.nUnits * x.unitPrice).asInstanceOf[Int]
   }
 
   object UnitCoutOrdering {
     given unitCountOrdering: Ordering[Purchase] with
-      override def compare(x: Purchase, y: Purchase): Int = y.nUnits -x.nUnits
+      override def compare(x: Purchase, y: Purchase): Int = y.nUnits - x.nUnits
   }
   object UnitPriceOrdering {
-    given  Ordering[Purchase] = Ordering.fromLessThan((x,y) => x.unitPrice < y.unitPrice)
+    given Ordering[Purchase] =
+      Ordering.fromLessThan((x, y) => x.unitPrice < y.unitPrice)
   }
 
-  val purchases: List[Purchase] = List(Purchase(3, 12.4), Purchase(5, 1), Purchase(2, 50.5))
+  val purchases: List[Purchase] =
+    List(Purchase(3, 12.4), Purchase(5, 1), Purchase(2, 50.5))
 
-  //import UnitCoutOrdering.unitCountOrdering
+  // import UnitCoutOrdering.unitCountOrdering
   import UnitPriceOrdering.given
   val sortedPurchases = purchases.sorted
   def main(args: Array[String]): Unit = {
     println(sortedPurchases)
     println(anOrderedList)
     println(sortedEmployees)
-   // import EmployeGivens.* //includes extension methods
+    // import EmployeGivens.* //includes extension methods
     println(Employee("sabuj", 43).greet)
   }
 
